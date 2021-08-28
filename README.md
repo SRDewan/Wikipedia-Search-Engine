e4a0fe5a5c76612bada55adc1b8e03fede7725db
SRDewan <shaurya.dewan@students.iiit.ac.in>
2021-08-29 01:00:59 +0530

    Done with basic setup for indexer. Just testing left and then suggested improvements as given in README.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Wikipedia-Search-Engine
 Search engine for wikipedia data dump.
+
+# To Do
+1. Fix references and external links parsing.
+2. Don't use lib for tokenization.
+3. Try creating own list of stop words using a program.
+4. Try Porter stemmer and lemitization.
+5. For index format, could try 1 line per field per word instead of per doc per word.
--- parser.py
+++ parser.py
@@ -1,4 +1,60 @@
 import xml.etree.ElementTree as ET
+import re
+import nltk
+from nltk.tokenize import word_tokenize as tokenizer
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+
+nltk.download('punkt')
+nltk.download('stopwords')
+stopWords = set(stopwords.words('english'))
+snowStemmer = SnowballStemmer(language='english')
+
+index = {}
+
+def fileWrite():
+    pass
+
+def processing(text):
+    text = text.lower()
+    tokens = tokenizer(text)
+    stemmedTokens = []
+
+    for token in tokens:
+        stemmed = snowStemmer(token)
+        if stemmed not in stopWords:
+            stemmedTokens.append(stemmed)
+
+    return stemmedTokens
+
+def bodyParse(body, id, title):
+    patterns = {
+            'title': None,
+            'infobox': re.compile('{{ ?Infobox ([\S\s]+)\n}}', re.I),
+            'category': re.compile('\[\[Category:(.+)\]\]', re.I),
+            'links': re.compile('== ?External Links ?==([\S\s]+)', re.I),
+            'references': re.compile('== ?References ?==([\S\s]+)[==|\[\[Category:|]', re.I),
+            }
+    ctr = 0
+
+    for key in patterns:
+        string = ""
+        if key != 'title':
+            for i in patterns[key].finditer(body):
+                string += '\n' + i.group(1)
+        else:
+            string = title
+
+        words = processing(string)
+        for word in words:
+            if word not in index:
+                index[word] = { id: [0, 0, 0, 0, 0, 0] }
+            elif id not in index[word]:
+                index[word][id] = [0, 0, 0, 0, 0, 0]
+
+            index[word][id][ctr] += 1
+        
+        ctr += 1
 
 def parse(file_path):
     docs = []
@@ -7,14 +63,14 @@ def parse(file_path):
         _, _, elem.tag = elem.tag.rpartition('}')
 
         if elem.tag == 'title':
-            docs.append({ 'title': elem.text });
+            docs.append({ 'title': elem.text })
 
         elif elem.tag == 'id' and 'id' not in docs[-1]:
-            docs[-1]['id'] = elem.text;
+            docs[-1]['id'] = elem.text
 
-        elif elem.tag == 'text':
-            docs[-1]['text'] = elem.text;
+        elif elem.tag == 'text' and elem.text != None:
+            bodyParse(elem.text, docs[-1]['id'], docs[-1]['title'])
 
         elem.clear()
 
-    return docs 
+    fileWrite()
